02672c0545d314eb18de234c0bc7e0345be8990f
Guilherme Henrique Crozariol <guicrozariol@gmail.com>
2024-01-10 15:45:28 -0300

    Create registration page
    Implement flow to create user, show a toast with a button to redirect user to the login page using useNavigate hook from react-router -dom

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,5 +1,6 @@
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
+import { Link } from 'react-router-dom'
 import { toast } from 'sonner'
 import { z } from 'zod'
 
@@ -37,6 +38,9 @@ export function SignIn() {
     <>
       <Helmet title="login" />
       <div className="p-8">
+        <Button variant={'ghost'} asChild className="absolute right-8 top-8">
+          <Link to="/sign-up">Create account</Link>
+        </Button>
         <div className="flex w-[350px] flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
@@ -46,7 +50,6 @@ export function SignIn() {
               Track your sales through the access panel
             </p>
           </div>
-
           <form onSubmit={handleSubmit(handleSignIn)} className="space-y-4">
             <div className="space-y-2">
               <Label htmlFor="email">E-mail</Label>
